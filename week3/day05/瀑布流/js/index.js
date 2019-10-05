62ca498013846478ae9c55caf2fe8ea9b35fed21
li091611 <374010745@qq.com>
2019-10-05 21:11:59 +0800

    更改

--- week3/day05/瀑布流/js/index.js
+++ week3/day05/瀑布流/js/index.js
@@ -3,17 +3,37 @@ function getData() {
     var xhr = new XMLHttpRequest();
     xhr.open('get', './data.json', true);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && /200|304/.test(status)) {
-            console.log(JSON.parse(xhr.response));
-            // render(data);
+        if (xhr.readyState == 4 && /200|304/.test(xhr.status)) {
+            // console.log(JSON.parse(xhr.response));
+            data = JSON.parse(xhr.response)
+            render(data);
         }
     }
     xhr.send()
 }
-getData()
-function render(data){
+// let n = 0;
+getData();
+let body = document.querySelector('.body'),
+    lis = body.getElementsByTagName('li');
+
+console.log(lis[2])
+let n = 0;
+function render(data) {
     let str = '';
-    data.forEach((item)=>{
-        let {pic,author,desc,height} = item
+    data.forEach((e,i) => {
+        let { pic, author, desc, height, id } = e;
+        str += `
+        <li>
+            <div class="img_box">
+                <img src="${pic}">
+                <p class="desc">${desc}</p>
+                <p class="author">${desc}</p>
+            </div>
+        </li>`
+        n++;
+        console.log(lis[n])
     })
+    
+    // console.log(n)
+    body.innerHTML = str;
 }
